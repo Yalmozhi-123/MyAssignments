00a2ad30167f18f22d9bedc4224066bf965a9faf
Yalmozhi-123 <142610636+Yalmozhi-123@users.noreply.github.com>
2023-08-18 21:00:12 +0530

    Add files via upload

---
+++ day2/Learnloop.java
@@ -0,0 +1,13 @@
+package Week2.day2;
+
+public class Learnloop {
+	
+	public static void main(String[] args) {
+		for (int i = 1; i < 20; i++) {
+			if(i%2!=0)
+				System.out.println(i);
+			
+		}
+	}
+
+}
